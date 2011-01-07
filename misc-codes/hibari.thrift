ba356fd5e6e7c30651d6c34498cf811310febfee
Joseph Wayne Norton <norton@alum.mit.edu>
2011-01-08 00:56:27 +0900

    Update copyright

--- misc-codes/hibari.thrift
+++ misc-codes/hibari.thrift
@@ -1,6 +1,6 @@
 #!/usr/local/bin/thrift --gen erl --gen java --gen py
 #----------------------------------------------------------------------
-# Copyright: (c) 2008-2010 Gemini Mobile Technologies, Inc.  All rights reserved.
+# Copyright (c) 2008-2011 Gemini Mobile Technologies, Inc.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
